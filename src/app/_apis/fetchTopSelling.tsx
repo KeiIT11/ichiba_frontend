b37d76a17e3672b39e498883fd232fda84b020e5
KeiIT11 <keiit596@gmail.com>
2023-10-01 21:07:24 +0900

    made PersonaCard, InfluencerCard

--- src/app/_apis/fetchTopSelling.tsx
+++ src/app/_apis/fetchTopSelling.tsx
@@ -3,7 +3,8 @@ export const fetchTopSelling = (genraID: number) => {
     //Temporarily showing example, replace this code.
     const topSelling: ReccomendedItemInfo = {
         itemCode: "kaguin:10084487",
-        imgUrl: "https://thumbnail.image.rakuten.co.jp/@0_mall/kaguin/cabinet/20230911_aio/7059727_ar-2__aio2.jpg?_ex=128x128",
+        imgUrl: "https://thumbnail.image.rakuten.co.jp/@0_mall/alude/cabinet/kagu/kagu03/ja1566.jpg?_ex=128x128",
+        // imgUrl: "/photo-1606107557195-0e29a4b5b4aa.jpeg",
         genraID: "566180",
         genraName: "ソファ"
     }
