3a3fefd0a786315e07cfbc45ca79ee1f0c344e96
Atharva Mahadeokar <athaarva999@gmail.com>
2023-10-13 07:04:56 +0530

    Added recommendation lists

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["thumbnail.image.rakuten.co.jp", "i.pinimg.com"],
+    domains: ["thumbnail.image.rakuten.co.jp", "i.pinimg.com","images.unsplash.com"],
   },
 };
 
--- src/app/_components/Header/navbar.tsx
+++ src/app/_components/Header/navbar.tsx
@@ -31,8 +31,36 @@ export const HomeNavBar = () => {
             className="input input-bordered w-[70%]"
           />
         </div>
-
         <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn ">
+              Recommmendation
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a onClick={() => router.push(`/recommendations/1`)}>
+                  New Grads
+                </a>
+              </li>
+              <li>
+                <a onClick={() => router.push(`/recommendations/2`)}>
+                  Relocators
+                </a>
+              </li>
+              <li>
+                <a onClick={() => router.push(`/recommendations/3`)}>Married</a>
+              </li>
+              <li>
+                <a onClick={() => router.push(`/recommendations/4`)}>
+                  Foreigners
+                </a>
+              </li>
+            </ul>
+          </div>
+
           <a className="btn" onClick={() => loginmodal.onOpen()}>
             Login
           </a>
